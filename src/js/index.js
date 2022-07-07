ef2f50aeef0289af96ba4e29de473a3e9fa2cd03
vadosxist@gmail.com <vadosxist@gmail.com>
2022-07-07 16:59:45 +0300

    HW Lesson5

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,18 @@
-import '../styles/main.scss';
+// Write a function that takes two numbers, say x and y, as arguments
+// Check if x is divisible by y
+// If yes, return x
+// If not, return the next higher natural number that is divisible by y
 
-console.log('test')
+function myFunction(x, y) {
+    if (x % y == 0) {
+        return x;
+    } else {
+        return myFunction(x + 1, y);
+    }
+}
+
+console.log(myFunction(1, 23));
+console.log(myFunction(23, 23));
+console.log(myFunction(7, 3));
+console.log(myFunction(-5, 7));
+
