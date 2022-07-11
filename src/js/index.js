e1db43b7dc3de40fe6256df0aa6d1ea5362a37b7
vadosxist@gmail.com <vadosxist@gmail.com>
2022-07-11 15:14:50 +0300

    HW Lesson6

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,39 @@
-import '../styles/main.scss';
+const pug = {
+    breed: "Pug",
+    name: "Phoebe",
+    age: 3,
+    sex: "female",
+    growth: 30,
+    color: "сappuccino",
+    trained: true,
+    dogsBark() {
+        console.log("woof-woof!");
+    },
+    sleeping() {
+        console.log("snore...");
+    },
+    jump() {
+        console.log("jumps somewhere");
+    }
+}
 
-console.log('test')
+function Dog(breed, name, age, sex, growth, color, trained) {
+    this.breed = breed;
+    this.name = name;
+    this.age = age;
+    this.sex = sex;
+    this.growth = growth;
+    this.color = color;
+    this.trained = trained;
+    this.dogsBark = function () {
+        console.log("woof-woof!");;
+    };
+    this.sleeping = function () {
+        console.log("snore...");
+    };
+    this.jump = function () {
+        console.log("jumps somewhere");
+    };
+}
+
+const spitz = new Dog("Spitz", "Petty", 2, "female", 20, "white", false);
