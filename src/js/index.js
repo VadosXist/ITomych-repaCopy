54036d4b1747751770ae09f7a6b22a6d7ceb7bff
vadosxist@gmail.com <vadosxist@gmail.com>
2022-07-13 00:53:58 +0300

    HW Lesson9

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,19 @@
-import '../styles/main.scss';
+//Напишите функцию, которая принимает в качестве аргументов 2 объекта и проверяет, является ли первый аргумент прототипом второго
 
-console.log('test')
+const obj1 = {
+    a: 2
+}
+
+const obj2 = {
+    a: 2
+}
+
+const obj3 = Object.create(obj1)
+
+function isPrototype(a, b) {
+    return a.isPrototypeOf(b)
+}
+
+console.log(isPrototype(obj1, obj2)) // false;
+console.log(isPrototype(obj3, obj1)) // false;
+console.log(isPrototype(obj1, obj3)) // true;
